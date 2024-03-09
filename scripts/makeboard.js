7f6d96812af3359feb00388064524b176d7989b6
Daniel Kim <daniel.kmkyu@gmail.com>
2024-03-09 11:02:09 -0800

    Update dropdown options and redirect based on choice

--- scripts/makeboard.js
+++ scripts/makeboard.js
@@ -23,11 +23,23 @@ var title = document.getElementById('title-input').value;
 var body = document.getElementById('body-input').value;
 var choice = document.getElementById('choosedropbtn').textContent;
 
+if (choice == 'School Life'){
+  choice = 'schoolLife';
+} else if (choice == 'Career Resources'){
+  choice = 'career';
+} else if (choice == 'Buy & Sell'){
+  choice = 'buySell';
+} else if (choice == 'General'){
+  choice = 'general';
+} else {
+  choice = 'CHOOSE';
+}
+
 if (title === '' || body === '' || choice === 'CHOOSE') {
   alert('Please fill all the fields');
   return;
 }
 
-location.href = 'general.html';
+location.href = choice + '.html';
 }
 
