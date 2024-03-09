23caec40f4d3b22d1dcd2080e37e90ce33833e7e
Seogin Hong <hsihsj94@gmail.com>
2024-03-09 12:43:02 -0800

    Refactor form submission and add file input

--- scripts/makeboard.js
+++ scripts/makeboard.js
@@ -10,74 +10,80 @@ function changeButtonText(text) {
 
 
 function toggleDropdown() {
-document.getElementById('dropdown').classList.toggle('hidden');
+  document.getElementById('dropdown').classList.toggle('hidden');
 }
 
 function changeButtonText(text) {
-document.getElementById('choosedropbtn').textContent = text;
-toggleDropdown();
+  document.getElementById('choosedropbtn').textContent = text;
+  toggleDropdown();
 }
 
 function submitForm() {
   var title = document.getElementById('title-input').value;
   var body = document.getElementById('body-input').value;
   var choice = document.getElementById('choosedropbtn').textContent;
+  var inputFile = document.getElementById('myFile').value;
 
-  if (choice == 'School Life'){
+
+  if (choice == 'School Life') {
     choice = 'schoolLife';
     var thread = db.collection("threads");
 
     thread.add({
-    author: firebase.auth().currentUser.displayName,
-    category: choice,
-    description: body,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    title: title,
+      author: firebase.auth().currentUser.displayName,
+      category: choice,
+      description: body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      title: title,
+      image: inputFile,
     }).then(function () {
-    location.href = choice + '.html';
+      location.href = choice + '.html';
     });
-  } else if (choice == 'Career Resources'){
+  } else if (choice == 'Career Resources') {
     choice = 'career';
     var thread = db.collection("threads");
 
     thread.add({
-    author: firebase.auth().currentUser.displayName,
-    category: choice,
-    description: body,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    title: title,
+      author: firebase.auth().currentUser.displayName,
+      category: choice,
+      description: body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      title: title,
+      image: inputFile,
     }).then(function () {
-    location.href = choice + '.html';
+      location.href = choice + '.html';
     });
-  } else if (choice == 'Buy & Sell'){
+  } else if (choice == 'Buy & Sell') {
     choice = 'buySell';
     var thread = db.collection("threads");
 
     thread.add({
-    author: firebase.auth().currentUser.displayName,
-    category: choice,
-    description: body,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    title: title,
+      author: firebase.auth().currentUser.displayName,
+      category: choice,
+      description: body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      title: title,
+      image: inputFile,
     }).then(function () {
-    location.href = choice + '.html';
+      location.href = choice + '.html';
     });
-  } else if (choice == 'General'){
+  } else if (choice == 'General') {
     choice = 'general';
     var thread = db.collection("threads");
 
     thread.add({
-    author: firebase.auth().currentUser.displayName,
-    category: choice,
-    description: body,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    title: title,
+      author: firebase.auth().currentUser.displayName,
+      category: choice,
+      description: body,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      title: title,
+      image: inputFile,
     }).then(function () {
-    location.href = choice + '.html';
+      location.href = choice + '.html';
     });
   } else {
-      alert('Please fill all the fields');
-      return;
-    }
+    alert('Please fill all the fields');
+    return;
   }
+}
 
