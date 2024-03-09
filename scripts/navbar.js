d5c5c4f75411e156f4f7d60e2f3e624642dad3e3
Daniel Kim <daniel.kmkyu@gmail.com>
2024-03-09 13:41:57 -0800

    Add onclick event handlers to navbar buttons

---
+++ scripts/navbar.js
@@ -0,0 +1,16 @@
+function goToSchoolLife() {
+    window.location.href = "schoolLife.html";
+}
+
+function goToCareer() {
+    window.location.href = "career.html";
+}
+
+
+function goToGeneral() {
+    window.location.href = "general.html";
+}
+
+function goToBuySell() {
+    window.location.href = "buysell.html";
+}
