dcc560e4fc31f509007758cff31ecbfce6bdc3e2
Daniel Kim <daniel.kmkyu@gmail.com>
2024-03-09 00:21:42 -0800

    Update dropdown options and remove duplicate code

--- scripts/makeboard.js
+++ scripts/makeboard.js
@@ -2,9 +2,6 @@ function toggleDropdown() {
   document.getElementById('dropdown').classList.toggle('hidden');
 }
 
-function toggleDropdown() {
-  document.getElementById('dropdown').classList.toggle('hidden');
-}
 
 function changeButtonText(text) {
   document.getElementById('choosedropbtn').textContent = text;
