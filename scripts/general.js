ae69ce3ba9c66b0dad5695bc9aec15c6ef92373f
Shawn Rim <trim2@my.bcit.ca>
2024-03-09 10:23:21 -0800

    Add threads from database to general.html and delete unused files

---
+++ scripts/general.js
@@ -0,0 +1,29 @@
+function displayCardsDynamically(collection) {
+    let threadTemplate = document.getElementById("threadTemplate");
+
+    db.collection(collection)
+        .get()
+        .then((allThreads) => {
+            allThreads.forEach((doc) => {
+                var title = doc.data().title;
+                var likes = doc.data().likes.length;
+                var dislikes = doc.data().dislikes.length;
+                var timestamp = doc.data().timestamp;
+                var docID = doc.id;
+                let newThread = threadTemplate.content.cloneNode(true);
+
+                newThread.querySelector("#title").innerHTML = title;
+                newThread.querySelector("#timestamp").innerHTML = timestamp;
+                newThread.querySelector("#likes-count").innerHTML = likes;
+                newThread.querySelector("#dislikes-count").innerHTML = dislikes;
+                // newThread.querySelector("a").href =
+                //     "general.html?docID=" + docID;
+
+                document
+                    .getElementById(`threadPlaceholder`)
+                    .appendChild(newThread);
+            });
+        });
+}
+
+displayCardsDynamically("threads");
